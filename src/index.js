b3e4a89967491e7df263f6dbace6f76726656ea0
Jose Guadalupe Cortes Cruz <joseegccr@gmail.com>
2022-01-28 22:34:31 -0600

    Descargando info y creando nodos y enriqueciendo la info a la mitad

--- src/index.js
+++ src/index.js
@@ -3,4 +3,36 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const  baseUrl = "https://platzi-avo.vercel.app";
+
+//web api
+//Conectarnos al server
+window.
+    fetch(`${baseUrl}/api/avo`)
+    //procesar la respuesta y convertirla en JSON
+    .then((respuesta) => respuesta.json())
+    //JSON-> DATA -> Renderizar info browser
+    .then((responseJson) => {
+        const todoLosItems = [];
+         responseJson.data.forEach((item) => {
+             //crear imagen
+             const imagen = document.createElement("img");
+             //URL de la imagen
+             imagen.src = `${baseUrl}${item.image}`;   
+             //crear imagen
+             const title = document.createElement("h2");
+             title.textContent = item.name;
+
+             //crear precio
+             const price = document.createElement("div");
+             price.textContent = item.price;
+
+             const container = document.createElement("div");
+             container.append(imagen, title, price);
+
+             todoLosItems.push(container);
+
+        });    
+
+        document.body.append(...todoLosItems);
+    }); 
